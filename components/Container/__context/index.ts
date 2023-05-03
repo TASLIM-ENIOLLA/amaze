8e3f62b6b25a7ec5c70e31d95ab62e470571d747
TASLIM-ENIOLLA <taslim.eniolla@gmail.com>
2023-05-03 22:44:12 +0100

    Eleventh commit 03/05/2023

--- components/Container/__context/index.ts
+++ components/Container/__context/index.ts
@@ -1,4 +1,4 @@
 import { createContext } from 'react'
 import { ContainerContextInterface } from '../__interface'
 
-export const ContainerContext = createContext<ContainerContextInterface | undefined>()
+export const ContainerContext = createContext<ContainerContextInterface | any>()
